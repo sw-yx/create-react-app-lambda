4699996c09a38a0b65b6624bfa5238c9d2716eda
sw-yx <swyx@swyx.io>
2018-09-14 15:03:15 +0800

    no spread

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -6,6 +6,6 @@ export function handler(event, context, callback) {
   console.log(event);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: "Hello, World!", ...test })
+    body: JSON.stringify({ msg: "Hello, World!", test })
   });
 }
